ce0153044ce3d51cc1db6a7ef6f6a1671d13cd87
870712pokohu <870712pokohu@gmail.com>
2023-12-22 17:22:23 -0500

    add back the api file

---
+++ api/Program.cs
@@ -0,0 +1,28 @@
+using DataLayer.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+var dbConnectionString = builder.Configuration.GetConnectionString("MySQL_Connection_String");
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// configure application to use MySQL Database
+builder.Services.AddDbContext<AppDbContext>(option =>{
+   option.UseMySql(dbConnectionString,ServerVersion.AutoDetect(dbConnectionString)); 
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
+
